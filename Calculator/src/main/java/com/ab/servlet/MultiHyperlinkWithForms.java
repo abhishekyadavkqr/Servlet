b677d8fcda15ab5d84ad227a25aaacf6e73f4df6
Dark Energy <Dark Energy@DarkEnergy>
2024-03-11 00:27:59 +0530

    Calculator App...

---
+++ Calculator/src/main/java/com/ab/servlet/MultiHyperlinkWithForms.java
@@ -0,0 +1,31 @@
+package com.ab.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MultiHyperlinkWithForms  extends HttpServlet {
+
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+			PrintWriter pw = res.getWriter();
+			res.setContentType("text/html");
+			
+			pw.println("<h1>hello</h1>");
+			pw.close();
+	}
+	
+	
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doGet(req, res);
+	}
+	
+	
+}
